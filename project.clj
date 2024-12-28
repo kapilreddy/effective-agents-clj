7e2b50feaef9b7ef5d9fe5a72bcbcb7e9b6f41b1
Kapil Reddy <reddy.kapil@gmail.com>
2024-12-28 12:24:57 +0530

    Update project.clj
    - Add license
    - Add instructor-clj dep

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject effective-agents-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :description "A repository of various LLM and agent workflows"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojars.kapil/instructor-clj "0.0.1-alpha.2"]]
   :repl-options {:init-ns effective-agents-clj.core})
