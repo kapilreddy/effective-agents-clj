e7619d64d7513a012f55c89ee6132e98d691e99d
Kapil Reddy <reddy.kapil@gmail.com>
2025-01-06 17:53:41 +0530

    Update instructor-clj to latest

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojars.kapil/instructor-clj "0.0.1-alpha.2"]]
+                 [org.clojars.kapil/instructor-clj "0.0.1-alpha.3"]]
   :repl-options {:init-ns effective-agents-clj.core})
