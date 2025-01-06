a04ef4cc75dd1bdce5cecc600b494624876da0f8
Kapil Reddy <reddy.kapil@gmail.com>
2025-01-06 18:53:58 +0530

    Update Clojure version to latest

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "A repository of various LLM and agent workflows"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojars.kapil/instructor-clj "0.0.1-alpha.3"]]
   :repl-options {:init-ns effective-agents-clj.core})
